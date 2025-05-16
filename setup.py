249f6a5788d43a832c6c5061b436cdd18a204c0a
jasonhpe <123524745+jasonhpe@users.noreply.github.com>
2025-05-16 10:36:18 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="LogViewer",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "jinja2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "LogViewer = LogViewer.__main__:main",
+        ],
+    },
+    package_data={
+        "LogViewer": ["templates/viewer_template.html"]
+    },
+    author="Jason Rojas",
+    description="Aruba LogViewer with GUI and web interface",
+)
