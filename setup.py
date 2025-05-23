c956d47dfb2fc47c7403f2e9033a971e188ea78c
jasonhpe <123524745+jasonhpe@users.noreply.github.com>
2025-05-23 06:21:50 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='LogViewer',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'jinja2',
+        'streamlit',
+        'pandas',
 
       
     ],
